3ff43aaa19767fd416a91b212f7ae68ae1eafb4b
Singh-Harsidak <harsidak24@gmail.com>
2023-07-04 19:13:53 -0400

    did not like the output of z, used the .format string method to change it and make it more cohesive

--- turntable.py
+++ turntable.py
@@ -3,7 +3,7 @@ counter = 0
 for i in range(10):
     x = random.randint(1,10)
     y = random.randint(1,10)
-    z = "What is", str(x), "into", str(y)
+    z = "What is {} into {}?".format(x,y)
     ans = int(input(z))
     if ans == (x*y):
         counter +=1
