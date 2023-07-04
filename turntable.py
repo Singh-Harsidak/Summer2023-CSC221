3b1d523cb646ac7f077c6657dc77dca12afa4f70
Singh-Harsidak <harsidak24@gmail.com>
2023-07-04 19:10:18 -0400

    print statement added and if, else statement added for fina score

--- turntable.py
+++ turntable.py
@@ -3,8 +3,15 @@ counter = 0
 for i in range(10):
     x = random.randint(1,10)
     y = random.randint(1,10)
-    ans = int(input("What is", str(x), "into", str(y)))
+    z = "What is", str(x), "into", str(y)
+    ans = int(input(z))
     if ans == (x*y):
         counter +=1
 
-        
+print("Your final score is", counter)
+
+if counter>5:
+    print("Good job!")
+else:
+    print("Try again!")
+
