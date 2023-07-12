616a55d69825ed585d9299c2f795d4ed4689a427
Harsidak singh <harsidak24@gmail.com>
2023-07-12 09:53:14 -0400

    Change use of range in drawFace()

--- sheet3/prettypictures.py
+++ sheet3/prettypictures.py
@@ -5,26 +5,16 @@ begin_graphics()
 
 def drawFace(x,y):
     Circle((x,y),40, color="yellow", filled=True)
-
-
-
     Circle((x-15,y), 5, color="black", filled=True)
     Circle((x+15,y), 5, color="black", filled=True)
-
     Line((x,y+10),(x-10,y-10))
-
     Line((x-10,y-10),(x+10,y-10))
-
-    Arc((x,y), 30, 225, 315, color="red", filled=True)
-
-
+    Arc((x,y), 30, 225, 315, color="red")
     Arc((x-40,y), 10, 90,270)
     Arc((x+40,y), 10, -90, 100)
 
-for row in range(8):
-    for col in range(9):
-        x = col *100
-        y = row *100
-        drawFace(x,y)
+for col in range(40, 581, 80):
+    for row in range(40, 440, 80):
+        drawFace(col, row)
 update_when('key_pressed') 
 end_graphics()
