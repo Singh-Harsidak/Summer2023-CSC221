47780f11e5515b9fc5989fa797844347a4e9637d
Harsidak singh <harsidak24@gmail.com>
2023-07-12 11:05:46 -0400

    Challenge2

--- sheet4/higherlower.py
+++ sheet4/higherlower.py
@@ -1,3 +1,8 @@
 from random import randint
 x = randint(1, 1000)
 print("I've thought of a number between 1 and 1000")
+guess = int(input("Make a guess: "))
+if(guess==x):
+    print("You guessed correct!")
+else:
+    print("Sorry, wrong guess")
