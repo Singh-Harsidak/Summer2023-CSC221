1540a706d6b3ad94ec7a430053ea9120f13bb364
Harsidak singh <harsidak24@gmail.com>
2023-07-12 11:23:56 -0400

    challenge5

--- sheet4/higherlower.py
+++ sheet4/higherlower.py
@@ -1,8 +1,18 @@
 from random import randint
 x = randint(1, 1000)     
 print("I've thought of a number between 1 and 1000")
-guess = int(input("Make a guess: "))
-if(guess==x):
-    print("You guessed correct!")
-else:
-    print("Sorry, wrong guess")
+estimates = 0 
+while True:
+    guess = int(input("Make a guess: "))
+    if(guess>x):
+        print("That's too high!")
+        estimates +=1
+    
+    elif(guess<x):
+        print("That's too low")
+        estimates +=1
+
+    else:
+        end = f"Correct! You guessed {estimates} times"
+        print(end)
+        break 
