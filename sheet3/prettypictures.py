396ffd419a8efadb64ae1940bc8ed990eb4c48d2
Harsidak singh <harsidak24@gmail.com>
2023-07-11 21:49:11 -0400

    Add Colors

--- sheet3/prettypictures.py
+++ sheet3/prettypictures.py
@@ -4,18 +4,18 @@ begin_graphics()
 
 
 def drawFace(x,y):
-    Circle((x,y),40)
+    Circle((x,y),40, color="yellow", filled=True)
 
 
 
-    Circle((x-15,y), 5)
-    Circle((x+15,y), 5)
+    Circle((x-15,y), 5, color="black", filled=True)
+    Circle((x+15,y), 5, color="black", filled=True)
 
     Line((x,y+10),(x-10,y-10))
 
     Line((x-10,y-10),(x+10,y-10))
 
-    Arc((x,y), 30, 225, 315)
+    Arc((x,y), 30, 225, 315, color="red", filled=True)
 
 
     Arc((x-40,y), 10, 90,270)
