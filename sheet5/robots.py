5941a3ab29d55d9787e9fe9e3198609b4700fb21
Harsidak singh <harsidak24@gmail.com>
2023-07-18 17:05:08 -0400

    Challenge5

--- sheet5/robots.py
+++ sheet5/robots.py
@@ -11,13 +11,41 @@ def place_player():
 def move_player():
     global player_x, player_y
     key = update_when('key_pressed')
-    if key == '6' and player_x < 63:
-        player_x += 1
+    if key == '1':
+        if player_x >0:
+            player_x -=1 
+        if player_y >0:
+            player_y -=1
+    elif key == '2' and player_y>0:
+        player_y -=1 
     elif key == '3':
         if player_x < 63:
             player_x += 1
         if player_y > 0:
             player_y -= 1
+    elif key == '4' and player_x>0:
+        player_x -=1
+
+    elif key == '6' and player_x < 63:
+        player_x += 1
+    
+    elif key == '7':
+        if player_x >0 :
+            player_x -= 1
+        if player_y < 47:
+            player_y += 1
+    
+    elif key == '8' and player_y < 47:
+        player_y += 1
+    
+    elif key == '9':
+        if player_x < 63:
+            player_x += 1
+        if player_y < 47:
+            player_y += 1
+    
+    
+    
     
     move_to(player_shape, (10 * player_x + 5, 10 * player_y + 5))
 
